39523b755289cf919984137db21104dbbcfef967
Field-Effect-Transitor <vshapoval270@gmail.com>
2026-08-03 13:54:24 +0300

    refactor(utils): enhance UniquePtr with noexcept and comparison operators

--- utils/include/UniquePtr.hpp
+++ utils/include/UniquePtr.hpp
@@ -1,7 +1,7 @@
 //  /utils/include/UniquePtr.hpp
 #pragma once
 
-#include <stddef.h>
+#include <cstddef>
 
 namespace Hermes {
 
@@ -18,16 +18,15 @@ namespace Hermes {
     >
     class UniquePtr {
         T*  _ptr;
-    
     public:
         UniquePtr(const UniquePtr&) = delete;    
-        UniquePtr(UniquePtr&& ptr) {
+        UniquePtr(UniquePtr&& ptr) noexcept {
             _ptr = ptr._ptr;
             ptr._ptr = nullptr;
         }    
 
         UniquePtr& operator=(const UniquePtr&) = delete;
-        UniquePtr& operator=(UniquePtr&& ptr) {
+        UniquePtr& operator=(UniquePtr&& ptr) noexcept {
             if (&ptr != this) {
                 DELETER{}(_ptr);
                 _ptr = ptr._ptr;
@@ -35,48 +34,45 @@ namespace Hermes {
             }
             return *this;
         }
-
-        UniquePtr() {
-            _ptr = nullptr;
+        UniquePtr& operator=(std::nullptr_t) noexcept {
+            reset();
+            return *this;
         }
 
-        explicit UniquePtr(T* ptr) {
-            _ptr = ptr;
+        explicit UniquePtr(T* ptr) noexcept { _ptr = ptr; }
+        UniquePtr() noexcept { _ptr = nullptr; }
+        UniquePtr(std::nullptr_t ptr) noexcept { _ptr = nullptr; }
+
+        ~UniquePtr() noexcept { DELETER{}(_ptr); }
+
+        T& operator*() noexcept { return *_ptr; }
+        T& operator*() const noexcept { return *_ptr; }
+
+        T* operator->() noexcept { return _ptr; }
+        T* operator->() const noexcept { return _ptr; }
+
+        explicit operator bool() const noexcept { return _ptr != nullptr; }
+        bool operator==(const UniquePtr& other) const noexcept {
+            return _ptr == other._ptr;
+        }
+        bool operator!=(const UniquePtr& other) const noexcept {
+            return _ptr != other._ptr;
         }
 
-        UniquePtr(nullptr_t ptr) {
-            _ptr = nullptr;
-        }
+        T* get() noexcept { return _ptr; }
+        T* get() const noexcept { return _ptr; }
 
-        ~UniquePtr() {
-            DELETER{}(_ptr);
-        }
-
-        T& operator*() {
-            return *_ptr;
-        }
-
-        T* operator->() {
-            return _ptr;
-        }
-
-        explicit operator bool() const {
-            return _ptr != nullptr;
-        }
-
-        T* get() {
-            return _ptr;
-        }
-
-        T* release() {
+        T* release() noexcept {
             T* ptr = _ptr;
             _ptr = nullptr;
             return ptr;
         }
 
-        void reset(T* new_ptr) {
-            DELETER{}(_ptr);
-            _ptr = new_ptr;
+        inline void reset(T* new_ptr = nullptr) noexcept {
+            if (new_ptr != _ptr) {
+                DELETER{}(_ptr);
+                _ptr = new_ptr;
+            }
         }
 
     };  //  class   UniquePtr
