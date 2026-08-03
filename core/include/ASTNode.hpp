158a171c2238e4cb03dc915e31c220f727192921
Field-Effect-Transistor <vshapoval270@gmail.com>
2026-08-04 00:39:28 +0300

    refactor(ast): migrate nodes to CRTP base, remove macros, and add structural equality

--- core/include/ASTNode.hpp
+++ core/include/ASTNode.hpp
@@ -6,37 +6,28 @@
 #include "Token.hpp"
 
 #include "utils/include/UniquePtr.hpp"
-
 #include "Vector.hpp"
-#include <memory>
-
-#define _ABI_SAFE_ASTNODE_DESTROYER  void destroy() override { delete this; }
-#define _ASTNODE_VISITOR_ACCEPT      Value accept(IVisitor& v) override { return v.visit(*this); }
 
 namespace Hermes {
 
-    struct LiteralNode : IASTNode {
+    struct LiteralNode : ASTNodeBase<LiteralNode> {
         Token   _token;
 
         LiteralNode(Token token) : _token(std::move(token)) {};
         ~LiteralNode() = default;
 
-        _ASTNODE_VISITOR_ACCEPT
-        _ABI_SAFE_ASTNODE_DESTROYER
-
+        bool compare(const LiteralNode& other) {
+        }
     };  //  struct  LiteralNode
 
-    struct VariableNode : IASTNode {
+    struct VariableNode : ASTNodeBase<VariableNode> {
         Token   _token;
 
         VariableNode(Token token) : _token(std::move(token)) {};
         ~VariableNode() = default;
+    };  //  struct  VariableNode
 
-        _ASTNODE_VISITOR_ACCEPT
-        _ABI_SAFE_ASTNODE_DESTROYER
-    };  //  struct VariableNode
-
-    struct UnaryNode : IASTNode {
+    struct UnaryNode : ASTNodeBase<UnaryNode> {
         Token       _token;
         ASTNodePtr  _child;
         
@@ -45,12 +36,9 @@ namespace Hermes {
             ASTNodePtr  child
         ): _token(std::move(t)), _child(std::move(child)) {};
         ~UnaryNode() = default;
-
-        _ASTNODE_VISITOR_ACCEPT
-        _ABI_SAFE_ASTNODE_DESTROYER
     };  //  struct  ASTUnanyNode
 
-    struct BinaryNode : IASTNode {
+    struct BinaryNode : ASTNodeBase<BinaryNode> {
         Token       _token;
         ASTNodePtr  _left;
         ASTNodePtr  _right;
@@ -62,27 +50,19 @@ namespace Hermes {
         ) : _token(std::move(t)), _left(std::move(left)), _right(std::move(right))  {}
 
         ~BinaryNode() = default;
+    };  //  struct  BinaryNode
 
-        _ASTNODE_VISITOR_ACCEPT
-        _ABI_SAFE_ASTNODE_DESTROYER
-    };  //  BinaryNode
-
-    struct FunctionNode : IASTNode {
+    struct FunctionNode : ASTNodeBase<FunctionNode> {
         Token              _token;
         Vector<ASTNodePtr> _args;
 
         FunctionNode(
-            Token                   token,
-            Vector<ASTNodePtr> args
+            Token               token,
+            Vector<ASTNodePtr>  args
         ) : _token(std::move(token)), _args(std::move(args)) {}
 
         ~FunctionNode() = default;
-
-        _ASTNODE_VISITOR_ACCEPT
-        _ABI_SAFE_ASTNODE_DESTROYER
     };  //  FunctionNode
 
 }   //  namespace   Hermes
 
-#undef  _ASTNODE_VISITOR_ACCEPT
-#undef  _ABI_SAFE_ASTNODE_DESTROYER
--- core/include/interface/IASTNode.hpp
+++ core/include/interface/IASTNode.hpp
@@ -11,7 +11,13 @@ namespace Hermes {
         virtual ~IASTNode() = default;
         virtual void destroy() = 0;
 
+        virtual const void* get_type_id() const = 0;
+        virtual bool is_equal(const IASTNode&) const = 0;
         virtual Value accept(IVisitor&) = 0;
+
+        friend bool operator==(const IASTNode& left, const IASTNode& right) {
+            return left.is_equal(right);
+        }
     };  //  struct  IAstNode
 
     struct ASTNodeDeleter {
@@ -23,5 +29,32 @@ namespace Hermes {
     };  //  struct ASTNodeDeleter
 
     using ASTNodePtr = UniquePtr<IASTNode, ASTNodeDeleter>;
+
+    template<typename DERIVED>
+    struct ASTNodeBase : public IASTNode {
+        inline static const char type{0};
+
+        virtual bool compare(const DERIVED&) const = 0;
+
+        const void* get_type_id() const override {
+            return static_cast<const void*>(&type);
+        }
+
+        bool is_equal(const IASTNode& other) const override {
+            if (this->get_type_id() != other.get_type_id()) {
+                return false;
+            }
+            const auto& other_derrived = static_cast<const DERIVED&>(other);
+            return static_cast<const DERIVED*>(this)->compare(other_derrived);
+        }
+
+        void destroy() override {
+            delete static_cast<DERIVED*>(this);
+        }
+
+        Value accept(IVisitor& v) override {
+            return v.visit(static_cast<DERIVED&>(*this));
+        }
+    };  //  struct  ASTNodeBase
 
 };  //  namespace   Hermes
